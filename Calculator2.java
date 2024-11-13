a3721af1d30b19d822ab20617921b498655d79eb
Ik1234567890 <ivchokolev007@gmail.com>
2024-11-13 08:56:46 +0200

    Calculator2.java

--- Calculator2.java
+++ Calculator2.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Calculator{
+public class Calculator2{
     public static void main(String[] args){
 
         // frame
